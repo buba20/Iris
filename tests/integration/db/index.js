1d43e958e15dbc6006ee3528137c19d38eeece92
Bartosz Szatanik <bszatanik@objectivity.co.uk>
2015-03-17 13:56:26 +0100

    Add test configuration and npm package installer

---
+++ tests/integration/db/index.js
@@ -0,0 +1,8 @@
+var t = require('../../db'),
+    expect = require('chai').expect;
+
+describe("Connect to database", function () {
+   it('Should return true',function(){
+       expect(true).to.equals(true);
+   });
+});
