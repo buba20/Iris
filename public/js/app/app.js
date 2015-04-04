27081eb97390f1c272d7a56913ce10b9a051f3e9
buba20 <buba20@gmail.com>
2015-04-04 19:06:10 +0200

    delete board directive

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -16,3 +16,27 @@ angular.module("IrisApp", ["ngRoute", "IrisApp.Controllers", "ui.bootstrap"])
             redirectTo: "/"
         });
     }]);
+
+
+angular.module("IrisApp")
+    .directive("delBoardBtn", ["$modal", "$location", "BoardService",
+        function ($modal, $location, boardService) {
+            return {
+                restrict: "E",
+                link: function (scope, element, attrs, controllers) {
+                    element.on('click', function (e) {
+                        $modal.open({
+                            templateUrl: "confirmDeleteModal.html",
+                            controller: "ModalInstanceCtrl"
+                        }).result.then(function () {
+                                boardService
+                                    .delete(attrs.boardId)
+                                    .success(function () {
+                                        $location.url("/board/");
+                                    });
+                            });
+                    });
+                },
+                templateUrl: "delBtnTmp.html"
+            }
+        }]);
